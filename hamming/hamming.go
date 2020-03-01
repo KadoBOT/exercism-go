e7a5b9c4231408f235aaaad76800abcf9f0e685f
KadoBOT <ricardo.ambrogi@gmail.com>
2020-03-01 10:30:09 +0100

    improve hamming answer

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -4,7 +4,7 @@ import "errors"
 
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
-		return 0, errors.New("DNAs have different size")
+		return -1, errors.New("DNAs have different size")
 	}
 
 	var dif int
