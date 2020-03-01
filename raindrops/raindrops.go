eaf23eb4667f092d58d40fbfcd72644a2947dc68
KadoBOT <ricardo.ambrogi@gmail.com>
2020-03-01 11:16:27 +0100

    improve raindrops

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,32 +1,21 @@
+
 package raindrops
 
-import (
-	"fmt"
-	"math"
-)
+import "strconv"
 
-type Num int
-func (n Num) hasRemainder(d int) bool {
-	fn, fd := float64(n), float64(d)
-	return fn >= fd && math.Remainder(fn, fd) == 0
-}
 
-func Convert(num int) string  {
-	var result string
-	n := Num(num)
-
-	if n.hasRemainder(3) {
+func Convert(num int) (result string)  {
+	if num % 3 == 0 {
 		result += "Pling"
 	}
-	if n.hasRemainder(5) {
+	if num % 5 == 0 {
 		result += "Plang"
 	}
-	if n.hasRemainder(7) {
+	if num % 7 == 0 {
 		result += "Plong"
 	}
-
 	if result == ""{
-		result = fmt.Sprint(num)
+		result = strconv.Itoa(num)
 	}
-	return result
+	return
 }
