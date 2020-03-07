15e98b3dcd038fe875509896b1a2bf6f7d5c522a
KadoBOT <ricardo.ambrogi@gmail.com>
2020-03-07 16:23:48 +0100

    robot-name

---
+++ robot-name/robot_name.go
@@ -0,0 +1,57 @@
+package robotname
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+type Robot struct {
+	name string
+}
+
+var letters = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var nums = []rune("0123456789")
+var usedNames string
+var max = 26 * 26 * 10 * 10 * 10
+var i int
+
+func createName() (string, error) {
+	if i > max {
+		return "", errors.New("max names exhausted")
+	}
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	name := make([]rune, 5)
+	name[0] = letters[r.Intn(len(letters))]
+	name[1] = letters[r.Intn(len(letters))]
+	name[2] = nums[r.Intn(len(nums))]
+	name[3] = nums[r.Intn(len(nums))]
+	name[4] = nums[r.Intn(len(nums))]
+	n := string(name)
+
+	if strings.Contains(usedNames, n) {
+		i++
+		return createName()
+	}
+
+	usedNames = fmt.Sprintf("%s%s", usedNames, n)
+	return string(name), nil
+}
+
+func (r *Robot) Name() (string, error) {
+	if r.name != "" {
+		return r.name, nil
+	}
+
+	name, err := createName()
+	r.name = name
+	return r.name, err
+}
+
+func (r *Robot) Reset() {
+	r.name = ""
+}
+
