3f9ba5ebecaef95b86d73a3672b16eb33aef9db9
KadoBOT <ricardo.ambrogi@gmail.com>
2020-03-15 12:12:59 +0100

    improve leap

--- leap/leap.go
+++ leap/leap.go
@@ -1,14 +1,5 @@
 package leap
 
 func IsLeapYear(year int) bool {
-	if year % 4 == 0 {
-		if year % 100 == 0 {
-			if year % 400 == 0 {
-				return true
-			}
-			return false
-		}
-		return true
-	}
-	return false
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
