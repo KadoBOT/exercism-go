647911c0b5e916ae28b11bdaa50a1ee011890f6d
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2020-03-21 11:10:43 +0100

    sieve (with formula and improvements)

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,24 +1,19 @@
 package sieve
 
 func Sieve(limit int) []int {
-	switch limit {
-	case 1:
-		return []int{}
-	default:
-		result := []int{}
-		nonPrime := make([]bool, limit+1)
+	result := []int{}
+	nonPrime := make([]bool, limit+1)
 
-		for i := 2; i <= limit; i++ {
-			ok := nonPrime[i]
-			if !ok {
-				result = append(result, i)
-				k := 0
-				for l := i * i; l <= limit; l = (i * i) + (k * i) {
-					nonPrime[l] = true
-					k++
-				}
+	for i := 2; i <= limit; i++ {
+		ok := nonPrime[i]
+		if !ok {
+			result = append(result, i)
+			k := 0
+			for l := i * i; l <= limit; l = (i * i) + (k * i) {
+				nonPrime[l] = true
+				k++
 			}
 		}
-		return result
 	}
+	return result
 }
