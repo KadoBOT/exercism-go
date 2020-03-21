a8f9dbfe60f2b27cb16a67c8548cd210d63f4552
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2020-03-21 11:01:50 +0100

    sieve (with formula)

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,23 +1,24 @@
 package sieve
 
+import "math"
+
 func Sieve(limit int) (result []int) {
 	switch limit {
 	case 1:
 		result = []int{}
-	case 2:
-		result = []int{2}
-	case 3:
-		result = []int{2, 3}
 	default:
-		result = []int{2, 3}
-		notPrime := map[int]bool{4: true, 6: true, 9: true}
-		for i := 4; i <= limit; i++ {
-			for _, n := range result {
-				notPrime[n*n] = true
-				notPrime[n*i] = true
-			}
-			if ok, _ := notPrime[i]; !ok {
+		nonPrime := map[int]bool{}
+
+		for i := 2; i <= limit; i++ {
+			ok, _ := nonPrime[i]
+			if !ok {
 				result = append(result, i)
+				k := 0
+				j := int(math.Pow(float64(i), 2))
+				for l := j; l <= limit; l = j + (k * i) {
+					nonPrime[l] = true
+					k++
+				}
 			}
 		}
 	}
