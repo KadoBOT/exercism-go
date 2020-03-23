fa6ad1d411639671650b2b66b05003ff69906366
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2020-03-23 06:28:37 +0100

    pascals_triangle.go impr

--- pascals-triangle/pascals_triangle.go
+++ pascals-triangle/pascals_triangle.go
@@ -1,27 +1,14 @@
 package pascal
 
-func factorial(number int) uint64 {
-	var result uint64 = 1
-	if number < 0 {
-
-	} else {
-		for i := 1; i <= number; i++ {
-			result *= uint64(i)
-		}
-	}
-	return result
-}
-
 func Triangle(rows int) [][]int {
 	result := make([][]int, rows)
 	for i := 0; i < rows; i++ {
-		row := make([]int, i+1)
-		rowFac := factorial(i)
-		for j := 0; j <= i; j++ {
-			colFac := factorial(j)
-			row[j] = int(rowFac / (colFac * factorial(i-j)))
+		result[i] = make([]int, i+1)
+		result[i][0] = 1
+		result[i][i] = 1
+		for j := 1; j < i; j++ {
+			result[i][j] = result[i-1][j-1] + result[i-1][j]
 		}
-		result[i] = row
 	}
 	return result
 }
